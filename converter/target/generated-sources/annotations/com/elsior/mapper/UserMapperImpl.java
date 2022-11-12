02589981e62c832f2ad7591c13ea39d7386b9319
Elimane Fofana <elimane.fofana@supinfo.com>
2022-11-12 08:02:42 +0100

    Maven Flatten Plugin. Closes #1

--- converter/target/generated-sources/annotations/com/elsior/mapper/UserMapperImpl.java
+++ converter/target/generated-sources/annotations/com/elsior/mapper/UserMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-12T07:03:03+0100",
+    date = "2022-11-12T07:47:56+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 18.0.2 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
